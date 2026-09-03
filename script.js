613cfb44f752670b8bbac087d06ed049abf686a7
Gugaa <joao_gm_godinho@estudante.sesisenai.org.br>
2026-09-03 15:38:59 -0300

    app de celular 1.0

--- script.js
+++ script.js
@@ -112,3 +112,9 @@ fetch(urlBusca)
 
   
 }
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(() => console.log('Service Worker registrado com sucesso!'))
+    .catch((error) => console.log('Falha ao registrar o Service Worker:', error));
+}
